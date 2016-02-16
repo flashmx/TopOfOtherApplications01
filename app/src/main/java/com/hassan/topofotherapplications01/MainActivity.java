b28c4259e3bd3f4a47e21c819faaf623d574b630
flashmx <flashmx11@gmail.com>
2016-02-16 23:11:34 +0300

    Change 2

--- app/src/main/java/com/hassan/topofotherapplications01/MainActivity.java
+++ app/src/main/java/com/hassan/topofotherapplications01/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 
 public class MainActivity extends Activity {
 
@@ -21,6 +22,8 @@ public class MainActivity extends Activity {
                 startService(intent);
             }
         });
+
+
     }
 
 }
