32875a80c395fd1a1137e9cec87dd0d2b33ab137
flashmx <flashmx11@gmail.com>
2016-02-16 22:35:43 +0300

    Change 1

--- app/src/main/java/com/hassan/topofotherapplications01/MainActivity.java
+++ app/src/main/java/com/hassan/topofotherapplications01/MainActivity.java
@@ -1,13 +1,26 @@
 package com.hassan.topofotherapplications01;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.button1).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ChatHeadService.class);
+                intent.putExtra("title", "Ciao");
+                intent.putExtra("text", "ChatHead");
+                startService(intent);
+            }
+        });
     }
+
 }
